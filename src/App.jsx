8e423d8bf00243abf6eb425b9fc97c61a6f34a04
Priyanshu Gupta <priyanshugupta1404@gmail.com>
2026-06-30 11:33:43 +0530

    UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
     try {
       // Basic fetch request to Google Gemini API
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
         {
           method: "POST",
           headers: {
